0d534e110937887955ecc7b2ab00a5afbcadc6fe
Fox-N-Paws <maddygracem@gmail.com>
2026-08-03 15:26:18 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,8 @@
-
 window.firebaseConfig = {
-    apiKey: "AIzaSyCYPyXIlMn5Z1PdJYjAFUehej4zWSpqSNU",
-    authDomain: "fox-and-paws.firebaseapp.com",
-    projectId: "fox-and-paws",
-    storageBucket: "fox-and-paws.firebasestorage.app",
-    messagingSenderId: "301598918304",
-    appId: "1:301598918304:web:4d8666bd469be155832bc5"
-}
+  apiKey: "AIzaSyCYPyXIlMn5Z1PdJYjAFUehej4zWSpqSNU",
+  authDomain: "fox-and-paws.firebaseapp.com",
+  projectId: "fox-and-paws",
+  storageBucket: "fox-and-paws.firebasestorage.app",
+  messagingSenderId: "301598918304",
+  appId: "1:301598918304:web:4d8666bd469be155832bc5"
+};
